aa93758de7eeec8099357a71589b6b8804963f34
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-11-23 11:30:15 +0530

    create nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,36 @@
+import logo from "../assets/images/Logo Horizontal R Blue@300x.png";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const navItems = [
+    { label: "About", href: "/about" },
+    { label: "Services", href: "/services" },
+    { label: "Our Team", href: "/our-team" },
+    { label: "Why EVs?", href: "/why-evs" },
+    { label: "Contact", href: "/contact" },
+  ];
+  return (
+    <>
+      <div className="navbar bg-white shadow-md rounded-b-xl w-full">
+        <div className="navbar-start">
+          <img src={logo} alt="logo" className="w-30" />
+        </div>
+        <div className="navbar-center">
+          {navItems.map((item, index) => (
+            <a
+              key={index}
+              className="text-md gap-4 p-2 pl-4 pr-4 cursor-pointer text-black hover:bg-zinc-100 rounded"
+            >
+              {item.label}
+            </a>
+          ))}
+        </div>
+        <div className="navbar-end gap-2 font-black">
+          <button className="btn btn-neutral btn-outline">Open Location</button>
+          <button className="btn btn-neutral btn-outline btn-circle"></button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
