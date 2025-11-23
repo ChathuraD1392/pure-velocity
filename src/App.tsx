50d96887ae1b0099270f89c8cc02583c24e0a586
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-11-23 08:05:02 +0530

    setup daisy ui and essential libraries

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
           Button
         </button>
+        <button className="btn btn-primary">Primary</button>
       </div>
     </>
   );
